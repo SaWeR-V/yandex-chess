2707dd1e8d8567f5c2459f115810b138c043a4b3
SaWeR <valera.sawer@yandex.ru>
2024-03-08 02:52:44 +0300

    Desktop page remastering

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
-const redLine = document.querySelector('.running-line');
+const redLines = document.querySelectorAll('.running-line');
 
 function infiniteScroll() {
     let step = 1;
     let timer = setInterval(() => {
         step++;
+        redLines.forEach(redLine => {
         redLine.style.transform = `translateX(${-1 * step}px)`;
 
         if (redLine.style.transform === 'translateX(-420px)') {
@@ -19,7 +20,7 @@ function infiniteScroll() {
             clearInterval(timer)
             infiniteScroll();
         }
-
+    })
     }, 10)
 
 }
@@ -31,4 +32,87 @@ function textInjection() {
     }
 }
 
-// infiniteScroll();
+infiniteScroll();
+
+const back = document.querySelector('.back');
+const next = document.querySelector('.next');
+const playersCounter = document.querySelector('.players-counter').querySelector('p');
+const players = document.querySelectorAll('.card');
+
+function playersCarousel() {
+    // const back = document.querySelector('.back');
+    // const next = document.querySelector('.next');
+    // const playersCounter = document.querySelector('.players-counter').querySelector('p');
+
+    // const players = document.querySelectorAll('.card');
+
+    next.addEventListener('click', () => {
+        for (let i = 0; i < players.length; i++) {
+            players[i].style.transform = `translateX(${-100 * 3}%)`;
+        }
+        back.disabled = false;
+        next.disabled = true;
+
+        playersCounter.firstChild.textContent = '6';
+        playersCounter.lastChild.classList.remove('semitransparent');
+    })
+
+    back.addEventListener('click', () => {
+        for (let i = 0; i < players.length; i++) {
+            players[i].style.transform = `translateX(0%)`;
+        }
+        back.disabled = true;
+        next.disabled = false;
+
+        playersCounter.firstChild.textContent = '3';
+        playersCounter.lastChild.classList.add('semitransparent');
+    })
+
+};
+
+function autoScrollDesktop() {
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].style.transform !== 'translateX(-300%)') {
+        
+            players[i].style.transform = `translateX(${-100 * 3}%)`;
+            
+            back.disabled = false;
+            next.disabled = true;
+
+            playersCounter.firstChild.textContent = '6';
+            playersCounter.lastChild.classList.remove('semitransparent');
+        }
+        else {
+            players[i].style.transform = `translateX(0%)`;
+            playersCounter.firstChild.textContent = '3';
+            playersCounter.lastChild.classList.add('semitransparent');
+
+            back.disabled = true;
+            next.disabled = false;
+        }
+    }
+};
+
+
+if (window.outerWidth > 375) {
+    playersCarousel();
+    setInterval(() => autoScrollDesktop(), 4000);
+}
+
+
+function scrollToBlock() {
+    const donateBtn = document.querySelector('.donate');
+    const learnMoreBtn = document.querySelector('.learn-more');
+    const explanationBlock = document.querySelector('.explanation');
+    const playersBlock = document.querySelector('.players');
+
+    donateBtn.addEventListener('click', () => {
+        explanationBlock.scrollIntoView({behavior: "smooth", inline: "start"})
+    })
+
+    learnMoreBtn.addEventListener('click', ()=> {
+        playersBlock.scrollIntoView({behavior: "smooth", inline: "start"})
+    })
+}
+
+scrollToBlock();
